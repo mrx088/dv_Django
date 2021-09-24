214a81420b43b1dba1891c2209e9b270fa0bee95
user <user@gmail.com>
2021-09-24 12:03:19 -0700

    Change setting

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -82,12 +82,8 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'onlineshop',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
